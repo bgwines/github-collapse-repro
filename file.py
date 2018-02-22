c701c783ac7aaa18e46af6e7d95364316f02f878
Brett Wines <bwines@quip.com>
2018-02-22 09:21:28 -0800

    Set up file

---
+++ file.py
@@ -0,0 +1,49 @@
+
+class C
+    def f(self)
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        # f's body
+        pass
+
+    async def g(self):
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        # g's body
+        pass
